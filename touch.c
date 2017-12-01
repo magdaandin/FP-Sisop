a791081a76b138a2dbcf4d3d7a7b49fe7b5e2a54
magdaandin <32873352+magdaandin@users.noreply.github.com>
2017-12-01 12:09:21 +0700

    dibetulkan

--- touch.c
+++ touch.c
@@ -6,20 +6,20 @@
 int
 main(int argc, char *argv[])
 {
-	if (argc <= 1) {
-		printf(1, "Please input this command as [touch file_name1 file_name2 ...]\n");
+	if (argc <2) {
+		printf(1, "input [touch file_name1 file_name2 ...]\n");
 	}
 
 	int i, count = 0;
 	for (i = 1; i < argc; i++) {		
-		int fd;
+		int a;
 		
 		if ((fd = open(argv[i], O_RDONLY)) < 0) {
 			
-			fd = open(argv[i], O_CREATE|O_RDONLY);
+			a = open(argv[i], O_CREATE|O_RDONLY);
 			count++;
 		}
-		close(fd);
+		close(a);
 	}
 
 	printf(1, "%d file(s) created, %d file(s) skiped.\n", count, argc - 1 - count);
