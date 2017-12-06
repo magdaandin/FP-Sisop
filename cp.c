f603b6782106497666ea56563e2574ebbfb1548e
magdaandin <magdalenaandin@gmail.com>
2017-12-06 13:40:00 +0700

    tambahan

--- cp.c
+++ cp.c
@@ -2,35 +2,146 @@
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
+#include "fs.h"
 
-char buf[512];
+#define BUF_SIZE 256
 
+void ls(char *path);
+char* fmtname(char *path);
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int fd0, fd1, n;
+	
+	if (argc !=3)
+	{
+		printf(1, "masukkan [cp src_file dest_file]\n");
+		exit();
+	}
+	
+	
+	int fd_src = open(argv[1], O_RDONLY);
+	if (fd_src == -1)
+	{
+		printf(1, "file asal tidak bisa dibuka\n");
+		exit();
+	}
+	
+	
+	struct stat st;
+	fstat(fd_src, &st);
+	if (st.type == T_DIR)
+	{
+		printf(1, "source file is a directory, the files in that directory is:\n");
+		ls(argv[1]);
+		printf(1, "the program can't open the file in that directory after list them.\n");
+		printf(1, "So, I'm sorry that you have to copy them one by one.\n");
+		exit();
+	}
 
-  if(argc <= 2){
-    printf(1, "butuh 2 arguments!\n");
-    exit();
-  }
+	
+	char com[128] = {};
+	strcpy(com, argv[2]);
+	int len1 = strlen(argv[1]);
+	int len2 = strlen(argv[2]);
+	if (argv[2][len2-1] == '/')
+	{
+		
+		int i = len1 - 1;
+		for (; i >= 0; i--)
+			if (argv[1][i] == '/')
+				break;
+		i++;
+		strcpy(&com[len2], &argv[1][i]);
+	}
+	
 
-  
-    if((fd0 = open(argv[1], O_RDONLY)) < 0){
-      printf(1, "cp: cannot open %s\n", argv[1]);
-      exit();
-    }
-if((fd1 = open(argv[2], O_CREATE|O_RDWR)) < 0){
-      printf(1, "cp: cannot open %s\n", argv[2]);
-      exit();
-    }
-    
-while(( n = read(fd0, buf, sizeof(buf))) > 0){
-	write (fd1, buf, n);
+	int fd_dest = open(com, O_WRONLY|O_CREATE);
+	if (fd_dest == -1)
+	{
+		printf(1, "create dest file failed\n");
+		exit();
+	}
+	
+	
+	char buf[BUF_SIZE] = {};
+	int len = 0;
+	while((len = read(fd_src, buf, BUF_SIZE)) > 0)
+		write(fd_dest, buf, len);
+	
+	
+	close(fd_src);
+	close(fd_dest);
+	exit();
 }
-    close(fd0);
-	 close(fd1);
-  
-  exit();
+
+char* fmtname(char *path)
+{
+	static char buf[DIRSIZ+1];
+	char *p;
+	
+	// Find first character after last slash.
+	for(p=path+strlen(path); p >= path && *p != '/'; p--)
+		;
+	p++;
+	
+	// Return blank-padded name.
+	if(strlen(p) >= DIRSIZ)
+		return p;
+	memmove(buf, p, strlen(p));
+	memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+	return buf;
+}
+
+void ls(char *path)
+{
+	char buf[512], *p;
+	int fd;
+	struct dirent de;
+	struct stat st;
+	
+	if((fd = open(path, 0)) < 0)
+	{
+		printf(2, "cannot open %s\n", path);
+		return;
+	}
+	
+	if(fstat(fd, &st) < 0)
+	{
+		printf(2, "cannot stat %s\n", path);
+		close(fd);
+		return;
+	}
+	
+	switch(st.type)
+	{
+		case T_FILE:
+		printf(1, "name = %s, type = file, size = %d\n", fmtname(path), st.size);
+		break;
+		
+		case T_DIR:
+		if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+		{
+			printf(1, "path too long\n");
+			break;
+		}
+		strcpy(buf, path);
+		p = buf+strlen(buf);
+		*p++ = '/';
+		
+		while(read(fd, &de, sizeof(de)) == sizeof(de))
+		{
+			if(de.inum == 0)
+				continue;
+			memmove(p, de.name, DIRSIZ);
+			p[DIRSIZ] = 0;
+			if(stat(buf, &st) < 0)
+			{
+				printf(1, "cannot stat %s\n", buf);
+				continue;
+			}
+			printf(1, "name = %s, type = directory, size = %d\n", fmtname(buf), st.size);
+		}
+		break;
+	}
+	close(fd);
 }
