e52bea1e2e03ed9d190827a412f0b136144e01b1
nurazizahk <33067364+nurazizahk@users.noreply.github.com>
2017-12-09 10:26:45 +0700

    Add files via upload

--- mv.c
+++ mv.c
@@ -30,8 +30,6 @@ int main(int argc, char *argv[])
 	fstat(fd_src, &st);
 	if (st.type == T_DIR)
 	{
-		printf(1, "source file is a directory, the files in that directory is:\n");
-		ls(argv[1]);
 		printf(1, "the program can't open the file in that directory after list them\n");
 		exit();
 	}
@@ -78,74 +76,4 @@ int main(int argc, char *argv[])
 	exit();
 }
 
-char* fmtname(char *path)
-{
-	static char buf[DIRSIZ+1];
-	char *p;
-	
-	
-	for(p=path+strlen(path); p >= path && *p != '/'; p--)
-		;
-	p++;
-	
-	
-	if(strlen(p) >= DIRSIZ)
-		return p;
-	memmove(buf, p, strlen(p));
-	memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
-	return buf;
-}
 
-void ls(char *path)
-{
-	char buf[512], *p;
-	int fd;
-	struct dirent de;
-	struct stat st;
-	
-	if((fd = open(path, 0)) < 0)
-	{
-		printf(2, "cannot open %s\n", path);
-		return;
-	}
-	
-	if(fstat(fd, &st) < 0)
-	{
-		printf(2, "cannot stat %s\n", path);
-		close(fd);
-		return;
-	}
-	
-	switch(st.type)
-	{
-		case T_FILE:
-		printf(1, "name = %s, type = file, size = %d\n", fmtname(path), st.size);
-		break;
-		
-		case T_DIR:
-		if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
-		{
-			printf(1, "path too long\n");
-			break;
-		}
-		strcpy(buf, path);
-		p = buf+strlen(buf);
-		*p++ = '/';
-		
-		while(read(fd, &de, sizeof(de)) == sizeof(de))
-		{
-			if(de.inum == 0)
-				continue;
-			memmove(p, de.name, DIRSIZ);
-			p[DIRSIZ] = 0;
-			if(stat(buf, &st) < 0)
-			{
-				printf(1, "cannot stat %s\n", buf);
-				continue;
-			}
-			printf(1, "name = %s, type = directory, size = %d\n", fmtname(buf), st.size);
-		}
-		break;
-	}
-	close(fd);
-}
